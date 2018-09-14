07b291e301e7329793900c5d4292048155d9fee6
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2018-09-14 16:17:34 +0200

    allowing exports to run for 10 minutes

--- aha.php
+++ aha.php
@@ -4,4 +4,6 @@ require 'vendor/autoload.php';
 use AhaApp\Aha;
 use AhaApp\AhaConfig;
 
+set_time_limit(10*60);
+
 (new Aha(new AhaConfig()))->run();
--- personio.php
+++ personio.php
@@ -4,5 +4,7 @@ require 'vendor/autoload.php';
 use PersonioApp\Personio;
 use PersonioApp\PersonioConfig;
 
+set_time_limit(10*60);
+
 (new Personio(new PersonioConfig()))->run();
 
